5e8c114ef04f751acd2fef3efa5642db8cf61412
somT-oss <somtochukwuuchegbu@gmail.com>
2021-06-20 19:32:36 +0100

    Email sending pushing

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def home_view(request):
-	return HttpResponse("Hello world")
-
-
+	send_mail(
+    'Subject here',
+    'Here is the message.',
+    settings.EMAIL_HOST_USER,
+    ['somtochukwuuchegbu@gmail.com'],
+)
+    return HttpResponse("Your email has been sent")
