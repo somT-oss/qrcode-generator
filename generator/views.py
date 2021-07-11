bb2aaaec0ae6a5d2ccb832e02d685fe5285fb2bd
somT-oss <somtochukwuuchegbu@gmail.com>
2021-07-11 22:12:17 +0100

    Merge confilct error solved

--- generator/views.py
+++ generator/views.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 
 def home_view(request):
-<<<<<<< HEAD
     email = send_mail("Account confirmation mail", f"Click on the link to activate your account",
                       settings.EMAIL_HOST_USER, ["somtochukwuuchegbu@gmail.com"])
     return render(request, "email.html", context={})
@@ -13,18 +12,3 @@ def home_view(request):
 
 def send_student_email(request):
     pass
-=======
-    return HttpResponse("Welcome home")
-
-
-def sending_email(request):
-    send_mail(
-    'Subject here',
-    'Here is the message.',
-    settings.EMAIL_HOST_USER,
-    ['somtochukwuuchegbu@gmail.com'],
-    fail_silently=False,
-)
-
-    return HttpResponse("Your mail has been sent")
->>>>>>> f762aa6bb518583a0b1ce0fa594f654d01ead543
