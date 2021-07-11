940900d5e75e05afda59d3933e70dd20bc90df67
somT-oss <somtochukwuuchegbu@gmail.com>
2021-07-11 22:22:49 +0100

    Error correction

--- qrcode_project/urls.py
+++ qrcode_project/urls.py
@@ -15,16 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static 
-from django.conf import settings 
+from django.conf.urls.static import static
+from django.conf import settings
 from generator.views import home_view, sending_email
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_view, name="home"),
-    path("send_mail", sending_email, name="send_mail"), 
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
